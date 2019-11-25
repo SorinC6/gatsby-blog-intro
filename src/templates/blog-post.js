b23ac8a242f8103bdcb0983739a5c70296be4df0
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 18:25:49 +0200

    added graphQL query for getting a specific post filtered by the slug

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React from "react";
+import { graphql } from "gatsby";
+import Layout from "../components/Layout";
 
-import Layout from '../components/Layout'
+export const query = graphql`
+  query($slug: String) {
+    allMarkdownRemark(filter: { fields: { slug: { eq: $slug } } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            date
+          }
+          html
+        }
+      }
+    }
+  }
+`;
 
-export function BlogPostTemplate({}) {
+export function BlogPostTemplate(props) {
+  //console.log(props);
   return (
     <>
-      <h1>Blog Post Page</h1>
+      <h1></h1>
     </>
-  )
+  );
 }
 
-BlogPostTemplate.propTypes = {}
+BlogPostTemplate.propTypes = {};
 
-function BlogPost() {
+function BlogPost(props) {
+  console.log(props);
   return (
     <Layout>
       <BlogPostTemplate />
     </Layout>
-  )
+  );
 }
 
-BlogPost.propTypes = {}
+BlogPost.propTypes = {};
 
-export default BlogPost
+export default BlogPost;
