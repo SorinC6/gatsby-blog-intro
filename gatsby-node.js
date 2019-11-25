b80dab08b53d0c5eff2c04b85146ee1f53f21526
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 17:30:15 +0200

    created pages for every post

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,18 +1,51 @@
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const path = require("path");
+const { createFilePath } = require("gatsby-source-filesystem");
+const { fmImagesToRelative } = require("gatsby-remark-relative-images");
 
-exports.createPages = () => {}
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions;
+
+  //Get the path to template
+  const blogTemplate = path.resolve("./src/templates/blog-post.js");
+
+  //Get markdown data
+  const res = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `);
+  //Create new Pages
+  res.data.allMarkdownRemark.edges.forEach(edge => {
+    //console.log(JSON.stringify(edge, undefined, 4));
+    createPage({
+      component: blogTemplate,
+      path: edge.node.fields.slug,
+      context: {
+        slug: edge.node.fields.slug
+      }
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
+  const { createNodeField } = actions;
+  fmImagesToRelative(node); // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
+    //console.log(JSON.stringify(node, undefined, 4));
+    const value = createFilePath({ node, getNode });
     createNodeField({
       name: `slug`,
       node,
-      value,
-    })
+      value
+    });
   }
-}
+};
