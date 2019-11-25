5c8991ec485b6e516932a66b0bad929555540f62
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 11:09:46 +0200

    added gloal styles

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,42 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import Header from "./Header";
 import { useSiteMetadata } from "../lib/hooks";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+      box-sizing: border-box;
+      margin: 0;
+    }
+    html,
+    body {
+      margin: 0;
+      color: #555;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+        'Segoe UI Emoji', 'Segoe UI Symbol';
+      font-size: 18px;
+      line-height: 1.4;
+        /* remove margin for the main div that Gatsby mounts into */
+      > div {
+          margin-top: 0;
+        }
+    }
+  
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      color: #222;
+      line-height: 1.1;
+
+      + * {
+      margin-top: 0.5rem;
+      }
+    }
+`;
 
 function Layout(props) {
   const {
@@ -24,6 +60,7 @@ function Layout(props) {
         <meta property="og:title" content={title} />
         <meta property="og:url" content="/" />
       </Helmet>
+      <GlobalStyle />
       <Header />
       <main>{props.children}</main>
     </div>
