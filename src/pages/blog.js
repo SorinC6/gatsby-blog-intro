7417e128833a7b97626649567a2470ecec87b568
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 11:14:16 +0200

    added blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Blog = () => {
+  return (
+    <Layout>
+      <p>Blog Page</p>
+    </Layout>
+  );
+};
+
+export default Blog;
