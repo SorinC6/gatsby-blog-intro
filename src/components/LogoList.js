54a2ec977473202597c27a3a3c3ed1492ebfb908
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 15:19:05 +0200

    added Costum Slider for Logos

--- src/components/LogoList.js
+++ src/components/LogoList.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Logo from "./Logo";
 import styled from "styled-components";
+import LogoSlider from "./LogoSlider";
 
 const LogoList = ({ logoData }) => {
   //console.log(logoData);
   return (
-    <Root>
-      {logoData &&
-        logoData.map((item, idx) => {
-          return <Logo key={idx} image={item.image} link={item.link} />;
-        })}
-    </Root>
+    <>
+      <Root>
+        {logoData &&
+          logoData.map((item, idx) => {
+            return <Logo key={idx} image={item.image} link={item.link} />;
+          })}
+      </Root>
+      <LogoSlider logoData={logoData} />
+    </>
   );
 };
 
@@ -22,8 +26,12 @@ const Root = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 100%;
-  height: 145px;
+  height: 100%;
   background: #f5f8fa 0% 0% no-repeat padding-box;
   margin-top: 231px;
   padding: 0 calc((100vw - 80vw) / 2);
+
+  @media (max-width: 400px) {
+    display: none;
+  }
 `;
