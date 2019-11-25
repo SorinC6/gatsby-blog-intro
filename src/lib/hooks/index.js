98258964dd27545612cfe80fa66670e5f600f367
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 17:36:15 +0200

    create graphQL query for getting the slug

--- src/lib/hooks/index.js
+++ src/lib/hooks/index.js
@@ -3,5 +3,13 @@ import useHeroData from "./useHeroData";
 import useIntroData from "./useIntroData";
 import useLogoData from "./useLogoData";
 import useBlogData from "./useBlogData";
+import useSlug from "./useSlug";
 
-export { useSiteMetadata, useHeroData, useIntroData, useLogoData, useBlogData };
+export {
+  useSiteMetadata,
+  useHeroData,
+  useIntroData,
+  useLogoData,
+  useBlogData,
+  useSlug
+};
