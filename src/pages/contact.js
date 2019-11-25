121760f012820e94e937143908523bdc09f3f61b
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 11:13:20 +0200

    added contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <p>Contact Page</p>
+    </Layout>
+  );
+};
+
+export default Contact;
