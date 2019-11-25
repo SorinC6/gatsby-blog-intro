6deeb3a2bf8c2eeb79b3ce03548d48b6cb676682
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 12:26:08 +0200

    added Blog List component

---
+++ src/components/BlogList.js
@@ -0,0 +1,33 @@
+import React from "react";
+// import BlogPreview from "./BlogPreview";
+import styled from "styled-components";
+
+const BlogList = ({ blogData }) => {
+  console.log(blogData);
+
+  return (
+    <Root>
+      {blogData &&
+        blogData.map(item => {
+          return (
+            // <BlogPreview
+            //   title={item.node.frontmatter.title}
+            //   excerpt={item.node.frontmatter.excerpt}
+            //   image={item.node.frontmatter.image}
+            // />
+          );
+        })}
+    </Root>
+  );
+};
+
+export default BlogList;
+
+const Root = styled.div`
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  width: 1440px;
+  margin-top: 200px;
+  /* padding: 0 120px; */
+`;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -9,18 +9,20 @@ import {
 import Hero from "../components/Hero";
 import Intro from "../components/Intro";
 import LogoList from "../components/LogoList";
+import BlogList from "../components/BlogList";
 
 export function IndexPageTemplate({}) {
   const heroData = useHeroData();
   const introData = useIntroData();
   const logoData = useLogoData();
   const blogData = useBlogData();
-  //console.log(logoData);
+  //console.log(blogData);
   return (
     <>
       <Hero heroData={heroData} />
       <Intro introData={introData} />
       <LogoList logoData={logoData} />
+      <BlogList blogData={blogData} />
     </>
   );
 }
