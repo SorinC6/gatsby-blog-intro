b0734daedbb87de4d372402cdc0e9310325103b6
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 10:42:08 +0200

    added index page

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import IndexTemplate from "../templates/index-page";
+
+const Index = () => {
+  return (
+    <>
+      <IndexTemplate />
+    </>
+  );
+};
+
+export default Index;
