7775169eb7b72d232a2859231a0a2a996caa33ec
Sorin Chis <kish.sorin@yahoo.com>
2019-11-25 18:52:01 +0200

    added dinamic content for every blog

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 import { graphql } from "gatsby";
 import Layout from "../components/Layout";
 
@@ -18,22 +19,26 @@ export const query = graphql`
   }
 `;
 
-export function BlogPostTemplate(props) {
+export function BlogPostTemplate({ title, date, body }) {
+  // not sure why post data its comming only in BlogPost component
   //console.log(props);
   return (
-    <>
-      <h1></h1>
-    </>
+    <Root>
+      <h1>{title}</h1>
+      <h6>{date}</h6>
+      <div dangerouslySetInnerHTML={{ __html: body }}></div>
+    </Root>
   );
 }
 
 BlogPostTemplate.propTypes = {};
 
-function BlogPost(props) {
-  console.log(props);
+function BlogPost({ data }) {
+  const { title, date } = data.allMarkdownRemark.edges[0].node.frontmatter;
+  const { html } = data.allMarkdownRemark.edges[0].node;
   return (
     <Layout>
-      <BlogPostTemplate />
+      <BlogPostTemplate title={title} date={date} body={html} />
     </Layout>
   );
 }
@@ -41,3 +46,28 @@ function BlogPost(props) {
 BlogPost.propTypes = {};
 
 export default BlogPost;
+
+const Root = styled.div`
+  padding: 0 calc((100vw - 600px) / 2);
+
+  h1,
+  h2 {
+    padding: 20px;
+    margin-top: 10px;
+  }
+
+  h6 {
+    text-align: center;
+  }
+
+  p {
+    padding: 20px 5px;
+    line-height: 1.4;
+    text-align: justify;
+    text-justify: inter-word;
+  }
+
+  img {
+    border-radius: 5px;
+  }
+`;
