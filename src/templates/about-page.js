47e4f8b1d8790a5715f54dff01c4035c92d5d591
Sorin Chis <kish.sorin@yahoo.com>
2019-11-26 17:13:10 +0200

    added subheading for intro content in configuration and graphQL query for about data

--- src/templates/about-page.js
+++ src/templates/about-page.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Root } from "../components/~common/basicPageStyles";
 import Layout from "../components/Layout";
+import { graphql } from "gatsby";
 
-export function AboutPageTemplate({}) {
+export const query = graphql`
+  query AboutQuery {
+    allMarkdownRemark(filter: { frontmatter: { title: { eq: "About us" } } }) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+          }
+          html
+        }
+      }
+    }
+  }
+`;
+
+export function AboutPageTemplate(props) {
+  console.log(props);
   return (
     <Root>
       <h1>About Page</h1>
@@ -12,7 +30,8 @@ export function AboutPageTemplate({}) {
 
 AboutPageTemplate.propTypes = {};
 
-function AboutPage() {
+function AboutPage(props) {
+  console.log(props);
   return (
     <Layout>
       <AboutPageTemplate />
